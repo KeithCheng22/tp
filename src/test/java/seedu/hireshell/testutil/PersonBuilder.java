ee30925ec6a24507b1ff08bcdb663520a7eb24ec
suhan0906 <125735654+suhan0906@users.noreply.github.com>
2026-03-12 23:04:01 +0800

    Fix style

--- src/test/java/seedu/hireshell/testutil/PersonBuilder.java
+++ src/test/java/seedu/hireshell/testutil/PersonBuilder.java
@@ -7,8 +7,8 @@ import seedu.hireshell.model.person.Email;
 import seedu.hireshell.model.person.Name;
 import seedu.hireshell.model.person.Person;
 import seedu.hireshell.model.person.Phone;
-import seedu.hireshell.model.person.Status;
 import seedu.hireshell.model.person.Rating;
+import seedu.hireshell.model.person.Status;
 import seedu.hireshell.model.tag.Tag;
 import seedu.hireshell.model.util.SampleDataUtil;
 
