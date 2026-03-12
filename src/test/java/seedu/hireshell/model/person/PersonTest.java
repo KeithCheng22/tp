5f60a87fa9601514b374cffa6122c32354f9915f
suhan0906 <125735654+suhan0906@users.noreply.github.com>
2026-03-13 01:39:43 +0800

    Fix checkstyle error

--- src/test/java/seedu/hireshell/model/person/PersonTest.java
+++ src/test/java/seedu/hireshell/model/person/PersonTest.java
@@ -94,7 +94,7 @@ public class PersonTest {
     public void toStringMethod() {
         String expected = Person.class.getCanonicalName() + "{name=" + ALICE.getName() + ", phone=" + ALICE.getPhone()
                 + ", email=" + ALICE.getEmail() + ", rating=" + ALICE.getRating()
-                + ", status=" + ALICE.getStatus() + ", tags=" + ALICE.getRoles() + "}";
+                + ", status=" + ALICE.getStatus() + ", roles=" + ALICE.getRoles() + "}";
         assertEquals(expected, ALICE.toString());
     }
 }
