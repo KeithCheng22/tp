f2c09d9adf8be19a9aba700fef4c810880e4965a
Chan Jan Hoe <e1398771@u.nus.edu>
2026-04-13 16:46:50 +0800

    Fix CheckStyle errors

--- src/main/java/seedu/hireshell/ui/PersonListPanel.java
+++ src/main/java/seedu/hireshell/ui/PersonListPanel.java
@@ -116,7 +116,7 @@ public class PersonListPanel extends UiPart<Region> {
         referralColumn.setCellValueFactory(cellData ->
                 new ReadOnlyStringWrapper(cellData.getValue().getReferralStatus().toString()));
 
-        dateColumn.setCellValueFactory(cellData ->         // 9. Date Column
+        dateColumn.setCellValueFactory(cellData -> // 9. Date Column
                 new ReadOnlyStringWrapper(cellData.getValue().getCreatedAt().toLocalDate().toString()));
 
         personTableView.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
